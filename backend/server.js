8e46aef57e2e2f8a201005f746a8e26e795026bf
ADITYAbasude <adityabasude13@gmail.com>
2023-12-30 20:27:26 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,14 @@ app.use(Express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-app.use(Express.static(`${__dirname}/client`));
+app.use(Express.static(`${__dirname}/frontend`));
 
 // deployment
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(Express.static(path.join(__dirname1, `/client/build`)));
+  app.use(Express.static(path.join(__dirname1, `/frontend/build`)));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, `client`, `build`, `index.html`));
+    res.sendFile(path.join(__dirname1, `frontend`, `build`, `index.html`));
   });
 } else {
   app.get("/", (req, res) => {
