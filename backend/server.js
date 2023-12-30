c0366221b27e112d40955d0d437a30c3d76d0635
ADITYAbasude <adityabasude13@gmail.com>
2023-12-30 20:21:47 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
 const db = require("./database");
 const cors = require("cors");
-const express = require("express");
+const Express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 db();
 
-const app = express();
+const app = Express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(Express.json());
 app.use(morgan("dev"));
 app.use(cors());
-``;
+
+app.use(Express.static(`${__dirname}/client`));
+
+// deployment
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(Express.static(path.join(__dirname1, `/client/build`)));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname1, `client`, `build`, `index.html`));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/seller", require("./routes/sellerAccountRegistration"));
 app.use("/api/product", require("./routes/products"));
--- backend/utils/paymentGateway.js
+++ backend/utils/paymentGateway.js
@@ -1,35 +1,31 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 const Razorpay = require("razorpay");
 const shortid = require("shortid");
 
-
-require("dotenv").config()
-
+require("dotenv").config();
 
 const instance = new Razorpay({
-    key_id: process.env.keyId,
-    key_secret: process.env.keySecret
-})
+  key_id: process.env.keyId,
+  key_secret: process.env.keySecret,
+});
 
+router.post("/createOrder", async (req, res) => {
+  const { amount, currency } = req.body;
+  try {
+    const order = {
+      amount: Number(amount),
+      currency: currency,
+      receipt: shortid.generate(),
+      payment_capture: 1,
+    };
 
-router.post('/createOrder', async (req, res) => {
-    const {amount , currency} = req.body
-    try {
-        const order = {
-            amount: Number(amount),
-            currency: currency,
-            receipt: shortid.generate(),
-            payment_capture: 1
-        }
+    const response = await instance.orders.create(order);
+    res.json(response);
+  } catch (e) {
+    res.status(400).json({ error: e });
+    console.log(e);
+  }
+});
 
-        const response = await instance.orders.create(order)
-        res.json(response)
-
-    } catch (e) {
-        res.status(400).json({ error: e })
-        console.log(e)
-    }
-})
-
-module.exports = router
+module.exports = router;
